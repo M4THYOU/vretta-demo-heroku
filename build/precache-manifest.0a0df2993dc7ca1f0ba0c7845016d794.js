fb6b8fda8bd9594332e8bc38ddfd42ae6ea54ca9
m4thyou <mathyou.wolfe@gmail.com>
2020-04-07 18:38:05 -0400

    I forgot the build folder

---
+++ build/precache-manifest.0a0df2993dc7ca1f0ba0c7845016d794.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d676c4fc7317235533226b5c61d30a6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "104be62b64bfb506de58",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "635be73b300e7b6a9307",
+    "url": "/static/css/main.e89b5612.chunk.css"
+  },
+  {
+    "revision": "104be62b64bfb506de58",
+    "url": "/static/js/2.ba8f75f3.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.ba8f75f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635be73b300e7b6a9307",
+    "url": "/static/js/main.fc265a2d.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "6caba2fab60c66ce198c63ca603d0d1f",
+    "url": "/static/media/when.6caba2fa.mp3"
+  }
+]);
